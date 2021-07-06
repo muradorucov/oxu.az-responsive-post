100baaee45dcac722776b034d1d80a328026651c
Murad685 <orucowmurad@gmail.com>
2021-07-06 16:04:22 +0400

    Update app.js Fayl

--- app.js
+++ app.js
@@ -1,8 +1,77 @@
-function like(id) {
-  id.innerHTML = "Like!";
-}
-function dislike(id) {
-  id.innerHTML = "DisLike!";
-  id.innerHTML = "DisLike!";
-}
+let post1likes = 145;
+  
+        function addLikepost1() {
+            post1likes = post1likes + 1;
+            document.querySelector('.total-likespost1').textContent
+                        = post1likes;
+        }
+  
+        // Simulate click function
+        function clickButton() {
+            document.querySelector('#btn1').click();
+        }
+        
+let post1dislikes = 37;
+  
+        function addDislikepost1() {
+            post1dislikes = post1dislikes + 1;
+            document.querySelector('.total-dislikespost1').textContent
+                        = post1dislikes;
+        }
+  
+        // Simulate click function
+        function clickButton() {
+            document.querySelector('#btn2').click();
+        }
+        
+let post2likes = 365;
+  
+        function addLikepost2() {
+            post2likes = post2likes + 1;
+            document.querySelector('.total-likespost2').textContent
+                        = post2likes;
+        }
+  
+        // Simulate click function
+        function clickButton() {
+            document.querySelector('#btn3').click();
+        }
+        
+let post2dislikes = 7;
+  
+        function addDislikepost2() {
+            post2dislikes = post2dislikes + 1;
+            document.querySelector('.total-dislikespost2').textContent
+                        = post2dislikes;
+        }
+  
+        // Simulate click function
+        function clickButton() {
+            document.querySelector('#btn4').click();
+        }
 
+let post3likes = 2856;
+  
+        function addLikepost3() {
+            post3likes = post3likes + 1;
+            document.querySelector('.total-likespost3').textContent
+                        = post3likes;
+        }
+  
+        // Simulate click function
+        function clickButton() {
+            document.querySelector('#btn4').click();
+        }
+        
+let post3dislikes = 387;
+  
+        function addDislikepost3() {
+            post3dislikes = post3dislikes + 1;
+            document.querySelector('.total-dislikespost3').textContent
+                        = post3dislikes;
+        }
+  
+        // Simulate click function
+        function clickButton() {
+            document.querySelector('#btn6').click();
+        }
